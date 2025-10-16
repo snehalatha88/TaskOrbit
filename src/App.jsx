eccf35f2143f5f494543fa391f15fa2487eee9e9
Sneha <snehakaturi12@gmail.com>
2025-10-16 11:09:53 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Taskform from "./Components/Taskform";
 import Tasklist from "./Components/Tasklist";
 import Progresstracker from "./Components/Progresstracker";
-function App(){
+import { useEffect,useState } from "react;"
+export default function App(){
+  const[tasks,setTasks]=useState([]);
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  });
+  const addTask=(task)=>{
+    setTasks(task);
+  }
   return(
     <div>
       <h1>Task Orbit</h1>
       <p>Our Friendly Task Orbit</p>
-      <Taskform />
-      <Tasklist />
+      <Taskform addTask={addTask}/>
+      <Tasklist tasks={tasks}/>
       <Progresstracker />
       <button>Clear all tasks</button>
     </div>
   )
 }
-export default App;
